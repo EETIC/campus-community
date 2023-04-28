ebf5790003b840317133acf465173277a42f0db4
Srutav Tarun <srutavtarun@gmail.com>
2023-04-28 23:12:13 +0530

    completed resources page

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,52 +1,196 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Resources.css";
 import NavBar from "../components/Navbar/NavBar";
 import Footer from "../components/Footer/Footer";
 
-import python_logo from "../images/python_logo.png";
-import circle_chevron_right from "../images/circle-chevron-right-solid.svg";
-
 function Resources() {
-  let py_desc =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phaselluslobortis hendrerit enim, at ornare est posuere vel. Aliquam velsemper mauris. Donec consectetur nec ex a tempus. Mauris eget dolornon orci varius fermentum. Phasellus ac felis turpis. Pellentesqueporttitor pharetra augue, mollis viverra dui. Praesent neque arcu,ultrices eu arcu at, facilisis convallis orci";
+  const softResources = [
+    {
+      title: "Python",
+      resources: [
+        "https://youtu.be/_uQrJ0TkZlc",
+        "https://www.youtube.com/playlist?list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n",
+      ],
+    },
+    {
+      title: "Machine Learning",
+      resources: [
+        "https://youtu.be/i_LwzRVP7bg",
+        "https://youtube.com/playlist?list=PL9ooVrP1hQOHUfd-g8GUpKI3hHOwM_9Dn",
+      ],
+    },
+    {
+      title: "Blender",
+      resources: [
+        "https://youtube.com/playlist?list=PLjEaoINr3zgFX8ZsChQVQsuDSjEqdWMAD",
+      ],
+    },
+    { title: "MATLAB", resources: ["https://youtu.be/T_ekAD7U-wU"] },
+    { title: "Stock Market", resources: ["https://youtu.be/XDtWSmnDLEs"] },
+    { title: "MS Excel", resources: [] },
+  ];
 
-  let drop_down_right = (
-    <img src={circle_chevron_right} alt="right" className="ddright" />
-  );
+  const hardResources = [
+    {
+      title: "Raspberry Pi",
+      resources : [ "https://www.raspberrypi.org/documentation/",
+       "https://www.youtube.com/watch?v=gpLzChnG1T8",]
+    },
+    {
+      title: "Arduino",
+      resources : [ "https://www.arduino.cc/",
+      
+        "https://www.youtube.com/playlist?list=PLliE3o7vC8rzZKcG-IPFz7s_4V0TzTf9X",]
+    },
+    {
+      title: "Robotics",
+      resources : [ "https://www.youtube.com/playlist?list=PLA2C19CC110C995AF",
+      
+        "https://www.robotshop.com/community/blog/show/top-5-robotics-websites-blogs",]
+    },
+    {
+      title: "Embedded Systems",
+      resources : [ "https://www.youtube.com/watch?v=BtrVGcFgMmw",
+       "https://www.edx.org/learn/embedded-systems",]
+    },
+    {
+      title: "PCB Design",
+      
+      resources : [  "https://www.youtube.com/playlist?list=PLy2022BX6EspFAK-F2doZRiL6H6E5xTmA",
+       "https://www.altium.com/resources",]
+    },
+    {
+      title: "3D Printing",
+      
+      resources : [  "https://www.youtube.com/playlist?list=PL8tYSbV_RTHwEokKfWG_yT8Tt_lZIvh1M",
+       "https://www.instructables.com/classes/technology/3d-printing/",]
+    },
+  ];
 
-  let ddright = document.querySelector(".ddright");
+  const [activeTab, setActiveTab] = useState("software");
 
-  let dropButton = document.querySelector(".ddbutton");
-  let isRotated = false;
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
 
-  if (ddright) {
-    dropButton.addEventListener("click", function () {
-      if (!isRotated) {
-        ddright.classList.add("rotated");
-      } else {
-        ddright.classList.remove("rotated");
+  useEffect(() => {
+    const soft = document.querySelector(".soft");
+    const hard = document.querySelector(".hard");
+
+    if (soft) {
+      soft.addEventListener("click", () => handleTabClick("software"));
+    }
+
+    if (hard) {
+      hard.addEventListener("click", () => handleTabClick("hardware"));
+    }
+
+    return () => {
+      if (soft) {
+        soft.removeEventListener("click", () => handleTabClick("software"));
       }
-      isRotated = !isRotated;
-      console.log(isRotated);
-    });
-  }
+
+      if (hard) {
+        hard.removeEventListener("click", () => handleTabClick("hardware"));
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const searchBar = document.getElementById("search-bar");
+    if (searchBar) {
+      searchBar.addEventListener("input", function () {
+        const searchString = searchBar.value.toLowerCase();
+        const resources = document.querySelectorAll(".block");
+
+        if (resources) {
+          resources.forEach((resource) => {
+            const topic = resource.dataset.topic.toLowerCase();
+            if (topic.includes(searchString)) {
+              resource.style.display = "block";
+            } else {
+              resource.style.display = "none";
+            }
+          });
+        }
+      });
+    }
+  }, []);
 
   return (
     <div>
       <NavBar />
-      <div className="block">
-        <div className="logo">
-          <img src={python_logo} alt="python-logo" />
+      <div className="resources">
+        <div className="top">
+          <p className="heading">Resources</p>
+          <input
+            type="text"
+            placeholder="Search topics"
+            id="search-bar"
+          ></input>
         </div>
-        <div className="content">
-          <div className="title">Python</div>
-          <div className="desc">{py_desc}</div>
-          <div className="mats">
-            <button type="button" className="ddbutton">
-              View Materials {drop_down_right}
-            </button>
+        <div className="buttons">
+          <button
+            className={`soft Button`}
+            id={`${activeTab === "software" ? "active" : ""}`}
+          >
+            Software
+          </button>
+          <button
+            className={`hard Button`}
+            id={`${activeTab === "hardware" ? "active" : ""}`}
+          >
+            Hardware
+          </button>
+        </div>
+        {activeTab === "software" && (
+          <div className="software">
+            {softResources.map((resource, index) => (
+              <div className="block" key={index} data-topic={resource.title}>
+                <div className="content">
+                  <div className="title">{resource.title}</div>
+                  <ul>
+                    {resource.resources.map((link, index) => (
+                      <li key={index}>
+                        <a
+                          href={link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {link}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
           </div>
-        </div>
+        )}
+        {activeTab === "hardware" && (
+          <div className="hardware">
+            {hardResources.map((resource, index) => (
+              <div className="block" key={index} data-topic={resource.title}>
+                <div className="content">
+                  <div className="title">{resource.title}</div>
+                  <ul>
+                    {resource.resources.map((link, index) => (
+                      <li key={index}>
+                        <a
+                          href={link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {link}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
       <Footer />
     </div>
