8daec0672d950aaf26cdb8d93da0489a4c3601ca
Srutav Tarun <srutavtarun@gmail.com>
2023-05-01 15:36:59 +0530

    added all resources

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -8,79 +8,384 @@ function Resources() {
     {
       title: "Python",
       resources: [
-        {head: "Programming with Mosh", link: "https://youtu.be/_uQrJ0TkZlc"},
-        {head: "Python tutorial for beginners", link:"https://www.youtube.com/playlist?list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n"},
+        { head: "Programming with Mosh", link: "https://youtu.be/_uQrJ0TkZlc" },
+        {
+          head: "ProgrammingKnowledge",
+          link: "https://www.youtube.com/playlist?list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n",
+        },
       ],
     },
     {
       title: "Machine Learning",
       resources: [
-        {head:"Machine Learning for Everybody", link: "https://youtu.be/i_LwzRVP7bg"},
-        {head:"Machine Learning Tutorial in Python | Edureka", link: "https://youtube.com/playlist?list=PL9ooVrP1hQOHUfd-g8GUpKI3hHOwM_9Dn"},
+        {
+          head: "freecodecamp",
+          link: "https://youtu.be/i_LwzRVP7bg",
+        },
+        {
+          head: "Edureka",
+          link: "https://youtube.com/playlist?list=PL9ooVrP1hQOHUfd-g8GUpKI3hHOwM_9Dn",
+        },
+        {
+          head: "Stanford University course",
+          link: "https://www.youtube.com/playlist?list=PLLssT5z_DsK-h9vYZkQkYNWcItqhlRJLN",
+        },
       ],
     },
     {
       title: "Blender",
       resources: [
-        {head: "Blender Beginner Donut Tutoria", link: "https://youtube.com/playlist?list=PLjEaoINr3zgFX8ZsChQVQsuDSjEqdWMAD"},
+        {
+          head: "Blender Guru",
+          link: "https://youtube.com/playlist?list=PLjEaoINr3zgFX8ZsChQVQsuDSjEqdWMAD",
+        },
       ],
     },
-    { title: "MATLAB", resources: [{head: "The Complete MATLAB Course", link: "https://youtu.be/T_ekAD7U-wU"}] },
-    { title: "EV", resources: [{head:"NexLoop" ,link: "https://youtu.be/XDtWSmnDLEs"}] },
-    { title: "Teachable Machine", resources: [{head: "teachable machine", link: "https://www.google.com/url?q=https://teachablemachine.withgoogle.com/train/image&hl=en-GB&sa=D&source=meet"}] },
-    {title: "D Zone chat box", resources: [{head: "d zhone chat box", link: "https://www.google.com/url?q=http://chat.doncjohn.in/&hl=en-GB&sa=D&source=meet"}]},
-    {title: "Code", resources: [{head: "jsdelivr", link: "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js"}]},
-    {title: "Tensorflow", resources: [{head: "Tensorflow user group", link: "https://www.meetup.com/tfug-thrissur/"}]},
-    {title: "Stock market", resources: [{head: "Launching the FREE Stock Market Course India Needed", liink: "https://youtu.be/XDtWSmnDLEs"}]},
-    {title: "MS Excel", resources: [{head: "Intellipaat", link: "https://youtu.be/27dxBp0EgCc"}]},
-    {title: "DevOps", resources: [{head: "Intellipaat", link: "https://youtu.be/JHoy3lDZOfY"}]},
-    {title: "Data Science", resources: [{head: "Intellipaat", link: "https://youtu.be/9l0DAYyJJhI"}]},
-    {title: "javascript", resources: [{head: "Clever Programmer", link: "https://youtube.com/playlist?list=PL9ooVrP1hQOHUfd-g8GUpKI3hHOwM_9Dn"}]},
-    {title: "Game programming", resources: [{head: "freecodecamp", link: "https://youtube.com/playlist?list=PLWKjhJtqVAbmqFs83T4W-FZQ9kK983tZC"}]},
-    {title: "Java", resources: [{head: "Apns College", link: "https://youtube.com/playlist?list=PLfqMhTWNBTe3LtFWcvwpqTkUSlB32kJop"}]},
-    {title: "Web Development", resources: [{head: "Apna College", link: "https://youtube.com/playlist?list=PLfqMhTWNBTe3H6c9OGXb5_6wcc1Mca52n"}]},
-    {title: "AI", resources: [{head: "Edureka", link: "https://youtube.com/playlist?list=PL9ooVrP1hQOGHNaCT7_fwe9AabjZI1RjI"}]},
-    {title: "Flutter", resources: [{head: "Net Ninja", link: "https://youtube.com/playlist?list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ"}]},
-    {title: "React", resources: [{head: "Codevolution", link: "https://youtube.com/playlist?list=PLC3y8-rFHvwgg3vaYJgHGnModB54rxOk3"}]},
-    {title: "AR", resources: [{head: "freecodecamp", link: "https://youtu.be/WzfDo2Wpxks"}]},
-    {title: ""}
+    {
+      title: "MATLAB",
+      resources: [
+        {
+          head: "Joseph Delgadillo",
+          link: "https://youtu.be/T_ekAD7U-wU",
+        },
+      ],
+    },
+    {
+      title: "EV",
+      resources: [{ head: "NexLoop", link: "https://youtu.be/XDtWSmnDLEs" }],
+    },
+    {
+      title: "Teachable Machine",
+      resources: [
+        {
+          head: "teachable machine",
+          link: "https://www.google.com/url?q=https://teachablemachine.withgoogle.com/train/image&hl=en-GB&sa=D&source=meet",
+        },
+      ],
+    },
+    {
+      title: "D Zone chat box",
+      resources: [
+        {
+          head: "d zhone chat box",
+          link: "https://www.google.com/url?q=http://chat.doncjohn.in/&hl=en-GB&sa=D&source=meet",
+        },
+      ],
+    },
+    {
+      title: "Code",
+      resources: [
+        {
+          head: "jsdelivr",
+          link: "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js",
+        },
+      ],
+    },
+    {
+      title: "Tensorflow",
+      resources: [
+        {
+          head: "Tensorflow user group",
+          link: "https://www.meetup.com/tfug-thrissur/",
+        },
+      ],
+    },
+    {
+      title: "Stock market",
+      resources: [
+        {
+          head: "marketfeed by ShariqueSamsudheen",
+          liink: "https://youtu.be/XDtWSmnDLEs",
+        },
+      ],
+    },
+    {
+      title: "MS Excel",
+      resources: [
+        { head: "Intellipaat", link: "https://youtu.be/27dxBp0EgCc" },
+      ],
+    },
+    {
+      title: "DevOps",
+      resources: [
+        { head: "Intellipaat", link: "https://youtu.be/JHoy3lDZOfY" },
+      ],
+    },
+    {
+      title: "Data Science",
+      resources: [
+        { head: "Intellipaat", link: "https://youtu.be/9l0DAYyJJhI" },
+      ],
+    },
+    {
+      title: "javascript",
+      resources: [
+        {
+          head: "Clever Programmer",
+          link: "https://youtube.com/playlist?list=PL9ooVrP1hQOHUfd-g8GUpKI3hHOwM_9Dn",
+        },
+        {
+          head: "freecodecamp",
+          link: "https://www.youtube.com/watch?v=PkZNo7MFNFg&list=PLWKjhJtqVAbleDe3_ZA8h3AO2rXar-q2V",
+        },
+      ],
+    },
+    {
+      title: "Game programming",
+      resources: [
+        {
+          head: "freecodecamp",
+          link: "https://youtube.com/playlist?list=PLWKjhJtqVAbmqFs83T4W-FZQ9kK983tZC",
+        },
+      ],
+    },
+    {
+      title: "Java",
+      resources: [
+        {
+          head: "Apns College",
+          link: "https://youtube.com/playlist?list=PLfqMhTWNBTe3LtFWcvwpqTkUSlB32kJop",
+        },
+      ],
+    },
+    {
+      title: "Web Development",
+      resources: [
+        {
+          head: "Apna College",
+          link: "https://youtube.com/playlist?list=PLfqMhTWNBTe3H6c9OGXb5_6wcc1Mca52n",
+        },
+      ],
+    },
+    {
+      title: "AI",
+      resources: [
+        {
+          head: "Edureka",
+          link: "https://youtube.com/playlist?list=PL9ooVrP1hQOGHNaCT7_fwe9AabjZI1RjI",
+        },
+      ],
+    },
+    {
+      title: "Flutter",
+      resources: [
+        {
+          head: "Net Ninja",
+          link: "https://youtube.com/playlist?list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ",
+        },
+      ],
+    },
+    {
+      title: "React",
+      resources: [
+        {
+          head: "Codevolution",
+          link: "https://youtube.com/playlist?list=PLC3y8-rFHvwgg3vaYJgHGnModB54rxOk3",
+        },
+      ],
+    },
+    {
+      title: "AR",
+      resources: [
+        { head: "freecodecamp", link: "https://youtu.be/WzfDo2Wpxks" },
+      ],
+    },
+    {
+      title: "OpenCV",
+      resources: [
+        {
+          head: "Installation",
+          link: "https://www.youtube.com/watch?v=CJXIjApHYVs&list=PLMoSUbG1Q_r_sc0x7ndCsqdIkL7dwrmNF",
+        },
+        {
+          head: "ProgrammmingKnowledge",
+          link: "https://www.youtube.com/playlist?list=PLS1QulWo1RIa7D1O6skqDQ-JZ1GGHKK-K",
+        },
+        {
+          head: "Python tutorial",
+          link: "https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html",
+        },
+        {
+          head: "Project: Gesture Volume Control",
+          link: "https://www.youtube.com/watch?v=9iEPzbG-xLE&list=PLMoSUbG1Q_r8jFS04rot-3NzidnV54Z2q",
+        },
+      ],
+    },
+    {
+      title: "C",
+      resources: [
+        {
+          head: "freecodecamp",
+          link: "https://www.youtube.com/watch?v=KJgsSFOSQv0&list=PLWKjhJtqVAbmUE5IqyfGYEYjrZBYzaT4m",
+        },
+      ],
+    },
+    {
+      title: "Deep Learning",
+      resources: [
+        {
+          head: "freecodecamp",
+          link: "https://www.youtube.com/watch?v=uuuzvOEC0zw&list=PLWKjhJtqVAbm5dir5TLEy2aZQMG7cHEZp&index=2",
+        },
+        {
+          head: "Neural Network",
+          link: "https://www.youtube.com/watch?v=aircAruvnKk",
+        },
+        {
+          head: "Deep Learning basics with Python, TensorFlow and Keras",
+          link: "https://www.youtube.com/playlist?list=PLQVvvaa0QuDfhTox0AjmQ6tvTgMBZBEXN",
+        },
+        {
+          head: "TensorFlow - Python Deep Learning Neural Network API",
+          link: "https://www.youtube.com/playlist?list=PLZbbT5o_s2xrwRnXk_yCPtnqqo4_u2YGL",
+        },
+        {
+          head: "Neural Networks - The Nature of Code",
+          link: "https://www.youtube.com/playlist?list=PLRqwX-V7Uu6aCibgK1PTWWu9by6XFdCfh",
+        },
+      ],
+    },
+    {
+      title: "Data Analysis with Python",
+      resources: [
+        {
+          head: "freecodecamp",
+          link: "https://www.youtube.com/watch?v=EsDFiZPljYo&list=PLWKjhJtqVAblvI1i46ScbKV2jH1gdL7VQ",
+        },
+      ],
+    },
+    {
+      title: "Unity",
+      resources: [
+        {
+          head: "Official Youtube channel",
+          link: "https://www.youtube.com/@unity/playlists",
+        },
+        {
+          head: "freecodecamp",
+          link: "https://www.youtube.com/watch?v=Sqb-Ue7wpsI&list=PLWKjhJtqVAbmh6kzCSUon-SoTaMvXnHMB",
+        },
+      ],
+    },
+    {
+      title: "Fusion",
+      resources: [
+        {
+          head: "Autodesk Fusion 360",
+          link: "https://www.youtube.com/@AutodeskFusion360/playlists",
+        },
+      ],
+    },
+    {
+      title: "Robotics Operating System",
+      resources: [
+        {
+          head: "Installation and Setup",
+          link: "https://www.youtube.com/watch?v=Qk4vLFhvfbI&list=PLLSegLrePWgIbIrA4iehUQ-impvIXdd9Q",
+        },
+        {
+          head: "Justin Huang",
+          link: "https://www.youtube.com/@JustinHuang101/videos",
+        },
+        {
+          head: "Shawn C",
+          link: "https://www.youtube.com/watch?v=ehtUb55Rmmg&list=PLk51HrKSBQ8-jTgD0qgRp1vmQeVSJ5SQC",
+        },
+      ],
+    },
+    {
+      title: "Linux",
+      resources: [
+        {
+          head: "Introduction",
+          link: "https://www.youtube.com/playlist?list=PLJcaPjxegjBV4aJh4hw3p3yL3CxE2sFXL",
+        },
+      ],
+    },
+    {
+      title: "Figma",
+      resources: [
+        {
+          head: "Figma Official Youtube Channel",
+          link: "https://www.youtube.com/watch?v=dXQ7IHkTiMM&list=PLXDU_eVOJTx7QHLShNqIXL1Cgbxj7HlN4&index=2",
+        },
+        {
+          head: "Flux Academy",
+          link: "https://www.youtube.com/watch?v=HZuk6Wkx_Eg",
+        },
+      ],
+    },
   ];
 
   const hardResources = [
     {
       title: "Raspberry Pi",
-      resources : [ {head: "Paul McWhorter", link:"https://www.youtube.com/playlist?list=PLGs0VKk2DiYypuwUUM2wxzcI9BJHK4Bfh"},]
+      resources: [
+        {
+          head: "Paul McWhorter",
+          link: "https://www.youtube.com/playlist?list=PLGs0VKk2DiYypuwUUM2wxzcI9BJHK4Bfh",
+        },
+      ],
     },
     {
       title: "Arduino",
-      resources : [ {head:"Arduino Lessons ",link: "https://www.youtube.com/playlist?list=PLGs0VKk2DiYx6CMdOQR_hmJ2NbB4mZQn-"},]
+      resources: [
+        {
+          head: "Arduino Lessons ",
+          link: "https://www.youtube.com/playlist?list=PLGs0VKk2DiYx6CMdOQR_hmJ2NbB4mZQn-",
+        },
+      ],
     },
     {
       title: "Beaglebone Black",
-      resources : [ {head: "Beaglebone Black LESSONS", link: "https://www.youtube.com/playlist?list=PLGs0VKk2DiYyThNvj6VyDFmOnQ8ncXk8b"},]
+      resources: [
+        {
+          head: "Beaglebone Black LESSONS",
+          link: "https://www.youtube.com/playlist?list=PLGs0VKk2DiYyThNvj6VyDFmOnQ8ncXk8b",
+        },
+      ],
     },
     {
       title: "NodeMcu",
-      resources : [ {head: "SM training academy", link: "https://www.youtube.com/watch?v=PSo3m7euzo0&list=PL_zvrXFdKgZpCgI1ZgfbJUcuGTKLL8pMT"},]
+      resources: [
+        {
+          head: "SM training academy",
+          link: "https://www.youtube.com/watch?v=PSo3m7euzo0&list=PL_zvrXFdKgZpCgI1ZgfbJUcuGTKLL8pMT",
+        },
+      ],
     },
     {
       title: "Atmel AVR",
-      resources : [ {head: "BuildYourCNC", link: "https://www.youtube.com/playlist?list=PLE72E4CFE73BD1DE1"},
-      {head: "humanHardDrive", link: "https://www.youtube.com/playlist?list=PLA6BB228B08B03EDD"}
-    ]
+      resources: [
+        {
+          head: "BuildYourCNC",
+          link: "https://www.youtube.com/playlist?list=PLE72E4CFE73BD1DE1",
+        },
+        {
+          head: "humanHardDrive",
+          link: "https://www.youtube.com/playlist?list=PLA6BB228B08B03EDD",
+        },
+      ],
     },
     {
       title: "ARM Architecture microcontrollers",
-      resources : [ {head: "ARM Microcontroller Programming With the TI Stellaris/Tiva-C Board(s)", link: "https://www.youtube.com/playlist?list=PLmfT_cdP5PYBWYvK_bCdGyBqQEiRzUPeq"},
-      {head:"MSP430 ", link: "https://www.youtube.com/playlist?list=PLRqKd7sGGKMTB5egcnNuR_SJjMgzFntKj"}
-    ]
+      resources: [
+        {
+          head: "ARM Microcontroller Programming With the TI Stellaris/Tiva-C Board(s)",
+          link: "https://www.youtube.com/playlist?list=PLmfT_cdP5PYBWYvK_bCdGyBqQEiRzUPeq",
+        },
+        {
+          head: "MSP430 ",
+          link: "https://www.youtube.com/playlist?list=PLRqKd7sGGKMTB5egcnNuR_SJjMgzFntKj",
+        },
+      ],
     },
-  ]
-  //   
+  ];
+  //
   //   {
   //     title: "Robotics",
   //     resources : [ "https://www.youtube.com/playlist?list=PLA2C19CC110C995AF",
-      
+
   //       "https://www.robotshop.com/community/blog/show/top-5-robotics-websites-blogs",]
   //   },
   //   {
@@ -90,13 +395,13 @@ function Resources() {
   //   },
   //   {
   //     title: "PCB Design",
-      
+
   //     resources : [  "https://www.youtube.com/playlist?list=PLy2022BX6EspFAK-F2doZRiL6H6E5xTmA",
   //      "https://www.altium.com/resources",]
   //   },
   //   {
   //     title: "3D Printing",
-      
+
   //     resources : [  "https://www.youtube.com/playlist?list=PL8tYSbV_RTHwEokKfWG_yT8Tt_lZIvh1M",
   //      "https://www.instructables.com/classes/technology/3d-printing/",]
   //   },
@@ -188,7 +493,7 @@ function Resources() {
                     <ul className="link">
                       {resource.resources.map((links, index) => (
                         <li key={index}>
-                          "{links.head}: "
+                          {links.head} :{" "}
                           <a
                             href={links.link}
                             target="_blank"
@@ -213,7 +518,7 @@ function Resources() {
                     <ul>
                       {resource.resources.map((links, index) => (
                         <li key={index}>
-                          {links.head} : {" "} 
+                          {links.head} :{" "}
                           <a
                             href={links.link}
                             target="_blank"
